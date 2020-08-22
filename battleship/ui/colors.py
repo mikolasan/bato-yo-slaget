4388643f378370840f32634427e72827dce9852e
Nikolay Neupokoev <neupokoev.n@gmail.com>
2020-08-21 21:19:31 -0700

    Move stuff around; delete old code

---
+++ battleship/ui/colors.py
@@ -0,0 +1,16 @@
+
+class Colors(object):
+
+    background = (235, 245, 238)
+    flat_back = (255, 179, 15)
+    flat_back_selected = (67, 80, 88)
+    font = (55, 50, 62)
+    font_selected = (235, 245, 238)
+
+    player_c = (70, 83, 98)
+    empty_c = (219, 219, 219)
+    near_c = (209, 209, 209)
+    fate_c = (255, 34, 12)
+    fog_c = (170, 170, 170)
+    miss_c = (255, 248, 240)
+    new_c = (67, 87, 173)
--- src/py/run-game.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# vim: set fileencoding=utf-8
-
-from battleship.Game import Game
-
-game = Game()
-game.play()
